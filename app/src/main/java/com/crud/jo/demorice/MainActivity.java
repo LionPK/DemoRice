e58b436fb67d42c4aaece0cd1c294ec21165bf2c
LionPK <praneed.kla@northbkk.ac.th>
2018-06-12 19:40:54 +0700

    login design

--- app/src/main/java/com/crud/jo/demorice/MainActivity.java
+++ app/src/main/java/com/crud/jo/demorice/MainActivity.java
@@ -1,13 +1,30 @@
 package com.crud.jo.demorice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        //link to Sing In
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),
+                        LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }

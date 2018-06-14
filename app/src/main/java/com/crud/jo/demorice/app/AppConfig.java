e6e72e50d3653f5e5aba288607cea4c203fd7b05
joza5944 <joza5944@gmail.com>
2018-06-15 00:27:30 +0700

    upcode pc

--- app/src/main/java/com/crud/jo/demorice/app/AppConfig.java
+++ app/src/main/java/com/crud/jo/demorice/app/AppConfig.java
@@ -3,8 +3,8 @@ package com.crud.jo.demorice.app;
 public class AppConfig {
 
     // Server user login url (ควรมีการเปลี่ยนตลอดถ้ายังไม่ได้เอาขึ้นโฮสติ้ง)
-    public static String URL_LOGIN = "http://10.10.10.95/Rice/android_login_api/login.php";
+    public static String URL_LOGIN = "http://192.168.3.107/Rice/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://10.10.10.95/Rice/android_login_api/register.php";
+    public static String URL_REGISTER = "http://192.168.3.107/Rice/android_login_api/register.php";
 }

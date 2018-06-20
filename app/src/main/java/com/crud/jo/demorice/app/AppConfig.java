c63cc13240644210f1c73eb32f19719bec651dfe
sarayut <tem456@hotmail.co.th>
2018-06-20 23:23:59 +0700

    dete

--- app/src/main/java/com/crud/jo/demorice/app/AppConfig.java
+++ app/src/main/java/com/crud/jo/demorice/app/AppConfig.java
@@ -3,22 +3,8 @@ package com.crud.jo.demorice.app;
 public class AppConfig {
 
     // Server user login url (ควรมีการเปลี่ยนตลอดถ้ายังไม่ได้เอาขึ้นโฮสติ้ง)
-<<<<<<< HEAD
     public static String URL_LOGIN = "www.projectricearea.com/android_login_api/login.php";
-
     // Server user register url
     public static String URL_REGISTER = "www.projectricearea.com/android_login_api/register.php";
-=======
-<<<<<<< HEAD
-    public static String URL_LOGIN = "http://www.projectricearea.com/android_login_api/login.php";
 
-    // Server user register url
-    public static String URL_REGISTER = "http://www.projectricearea.com/android_login_api/register.php";
-=======
-    public static String URL_LOGIN = "http://192.168.3.107/Rice/android_login_api/login.php";
-
-    // Server user register url
-    public static String URL_REGISTER = "http://192.168.3.107/Rice/android_login_api/register.php";
->>>>>>> de85794c6a07751b7d421438640189f91d791965
->>>>>>> b188c7038061928ae782ebbc0ecb8db7376e5901
 }

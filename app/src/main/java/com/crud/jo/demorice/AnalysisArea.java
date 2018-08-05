fefadaa681aa55bd4b8de489f1e50ed1fae87de7
joza5944 <joza5944@gmail.com>
2018-08-05 11:55:54 +0700

    หน้าวิเคราะห์

--- app/src/main/java/com/crud/jo/demorice/AnalysisArea.java
+++ app/src/main/java/com/crud/jo/demorice/AnalysisArea.java
@@ -15,17 +15,49 @@ public class AnalysisArea extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.content_analysis);
-            Spinner mySpinner = (Spinner) findViewById(R.id.spinnerrice);
-
-
+            Spinner mySpinnerrice = (Spinner) findViewById(R.id.spinnerrice);
+            Spinner mySpinnerarea = (Spinner) findViewById(R.id.spinnearea);
+            Spinner mySpinnertambon = (Spinner) findViewById(R.id.spinnetambon);
+            Spinner mySpinnerwater_area = (Spinner) findViewById(R.id.spinnerwater_area);
 
             ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(AnalysisArea.this,
                     android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.spinnerrice));
 
             myAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-            mySpinner.setAdapter(myAdapter);
+            mySpinnerrice.setAdapter(myAdapter);
+
+
+            ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(AnalysisArea.this,
+                    android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.spinnearea));
+
+            myAdapter2.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+            mySpinnerarea.setAdapter(myAdapter2);
+
+            ArrayAdapter<String> myAdapter3 = new ArrayAdapter<String>(AnalysisArea.this,
+                    android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.spinnetambon));
+
+            myAdapter3.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+            mySpinnertambon.setAdapter(myAdapter3);
+
+            ArrayAdapter<String> myAdapter4 = new ArrayAdapter<String>(AnalysisArea.this,
+                    android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.spinnerwater_area));
+
+            myAdapter4.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+            mySpinnerwater_area.setAdapter(myAdapter4);
+
+            Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar) ;
+            setSupportActionBar(toolbar);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext(),
+                            Users_Main.class);
+                    startActivity(i);
+                    finish();
+                }
+            });
 
     }
-    
+
     }
 
